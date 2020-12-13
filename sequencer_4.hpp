2d30adf4b89cceda93713dc9597cb6e7db506e8c
Lut99 <mullertim99@gmail.com>
2020-12-13 17:26:42 +0100

    Working on it

--- sequencer_4.hpp
+++ sequencer_4.hpp
@@ -4,7 +4,7 @@
  * Created:
  *   10/11/2020, 13:23:21
  * Last edited:
- *   10/11/2020, 14:50:39
+ *   13/12/2020, 17:14:08
  * Auto updated?
  *   Yes
  *
@@ -25,34 +25,71 @@
 
 /***** CONSTANTS *****/
 
+/* Index of the note lengths in the array of note statusses. */
+#define LENGTH 0
+/* Index of the note pitch in the array of note statusses. */
+#define PITCH 1
+/* Index of the note volume in the array of note statusses. */
+#define VELOCITY 2
+
+/* The default tempo (in BPM). */
+#define DEFAULT_TEMPO 120
+/* The standard length of each note (in the MIDI steps) */
+#define DEFAULT_LENGTH 1
+/* The standard pitch of each note (0-127) */
+#define DEFAULT_PITCH 48
+/* The standard velocity of each note (0-127) */
+#define DEFAULT_VELOCITY 127
 
 
 /***** PIN LAYOUTS *****/
 
-// First output pin for multiplexers 1 and 2
+/* Pin of the green led. */
+#define GREEN_LED 11
+
+/* First output pin for multiplexers 1 and 2. */
 #define MULT12_OUT1 2
-// Second output pin for multiplexers 1 and 2
+/* Second output pin for multiplexers 1 and 2. */
 #define MULT12_OUT2 3
-// Third output pin for multiplexers 1 and 2
+/* Third output pin for multiplexers 1 and 2. */
 #define MULT12_OUT3 4
-// First output pin for multiplexers 3 and 4
+/* First output pin for multiplexers 3 and 4. */
 #define MULT34_OUT1 5
-// Second output pin for multiplexers 3 and 4
+/* Second output pin for multiplexers 3 and 4. */
 #define MULT34_OUT2 6
-// Third output pin for multiplexers 3 and 4
+/* Third output pin for multiplexers 3 and 4. */
 #define MULT34_OUT3 7
-// Data pin for the first multiplexer, which is here used to read the potmeters
+/* Data pin for the first multiplexer, which is here used to read the potmeters. */
 #define MULT1_DATA A0
-// Data pin for the second multiplexer, which is here used to write LED values
+/* Data pin for the second multiplexer, which is here used to write LED values. */
 #define MULT2_DATA A1
-// Data pin for the third multiplexer, which is here used to read the control potmeters
+/* Data pin for the third multiplexer, which is here used to read the control potmeters. */
 #define MULT3_DATA A2
-// Data pin for the fourth multiplexer, which is here used flexibly
+/* Data pin for the fourth multiplexer, which is here used flexibly. */
 #define MULT4_DATA A3
 
 
 
 /***** HELPER FUNCTIONS *****/
+
+/* Sets a given multiplexer to the given index. */
+#define SET_MULTI(NAME, I) \
+    digitalWrite(MULT2_DATA, LOW); \
+    digitalWrite(NAME ## _OUT1, (I) & 0x1); \
+    digitalWrite(NAME ## _OUT2, ((I) >> 1) & 0x1); \
+    digitalWrite(NAME ## _OUT3, ((I) >> 2) & 0x1); \
+    digitalWrite(MULT2_DATA, HIGH);
+
+/* Converts a given number in BPM to a timeout in milliseconds. */
+#define BPM_TO_MS(BPM) \
+    (1000 / (((BPM) / 60.0) * 24.0))
+
+/* Helper macro that allows us to read N different booleans from a single N-bit number. */
+#define GET_BOOL(BOOL, I) \
+    ((bool) (((BOOL) >> (I)) & 0x1))
+/* Helper macro that allows us to write to N different booleans in a single N-bit number. */
+#define SET_BOOL(BOOL, I, VALUE) \
+    ((bool) ((BOOL) = (VALUE) ? (BOOL) | (1 << (I)) : (BOOL) & (~(1 << (I)))))
 
 
 
--- sequencer_4.ino
+++ sequencer_4.ino
@@ -4,7 +4,7 @@
  * Created:
  *   10/11/2020, 13:20:57
  * Last edited:
- *   10/11/2020, 15:12:39
+ *   13/12/2020, 17:26:05
  * Auto updated?
  *   Yes
  * 
@@ -25,6 +25,8 @@
 
 /***** GLOBALS *****/
 
+/* Led that is currently on. */
+uint8_t led;
 /* Used to keep track where we are in a bar (=all eight notes). */
 int8_t bar_pos;
 /* Used to keep track where we are in the music. */
@@ -32,6 +34,13 @@ uint32_t song_pos;
 
 /* Used to determine whether we need to move to the next beat. */
 unsigned long beat_clock;
+
+/* Used to keep track if a node is on. */
+uint8_t note_on;
+/* Used to keep track of how long a node is on. */
+uint8_t note_duration[8];
+/* Used to remember the status of the per-note potmeters, for the three different modes. */
+uint8_t note_stat[3][8];
 
 
 
@@ -44,12 +53,28 @@ void setup() {
     /* Step 1: Initialize constants. */
     
     // We start at the beginning of the bar & song
+    led = 0;
+    beat_clock = 0;
     bar_pos = -1;
     song_pos = 0;
 
 
 
-    /* Step 2: Setup pin layouts. */
+    /* Step 2: Initialize the note arrays. */
+
+    // Set all notes to off, initially
+    note_on = 0;
+    // Set each of the note_stat nested arrays
+    memset(note_stat[LENGTH], DEFAULT_LENGTH, 8);
+    memset(note_stat[PITCH], DEFAULT_PITCH, 8);
+    memset(note_stat[VELOCITY], DEFAULT_VELOCITY, 8);
+
+
+
+    /* Step 3: Setup pin layouts. */
+
+    // Setup the green led
+    pinMode(GREEN_LED, OUTPUT);
 
     // Setup out pins for multiplexer 1 and 2
     pinMode(MULT12_OUT1, OUTPUT);
@@ -60,6 +85,19 @@ void setup() {
     // Multiplexer 2 is used to write beat led outputs
     pinMode(MULT2_DATA, OUTPUT);
 
+    // Setup out pins for multiplexer 3 and 4
+    pinMode(MULT34_OUT1, OUTPUT);
+    pinMode(MULT34_OUT2, OUTPUT);
+    pinMode(MULT34_OUT3, OUTPUT);
+    // Multiplexer 3 is used to read control potmeters
+    pinMode(MULT3_DATA, INPUT);
+
+
+
+    /* Step 4: Set the LEDS on */
+    SET_MULTI(MULT34, 2);
+    digitalWrite(MULT2_DATA, HIGH);
+
     // Initialize the serial print
     Serial.begin(9600);
 
@@ -67,40 +105,72 @@ void setup() {
 
 /* Function that is called repeatedly by the Arduino as main loop. */
 void loop() {
+    // Get the current time
     unsigned long now = millis();
 
     /* Step 1: Increment the beat position if we need to. */
+    bool pos = false;
     bool beat = false;
-    if (bar_pos == -1 || now - beat_clock >= 1000 / ((120 / 60.0) * 24.0)) {
+    if (bar_pos == -1 || now - beat_clock >= BPM_TO_MS(DEFAULT_TEMPO)) {
+        // Don't forget to advance the clock
+        if (bar_pos >= 0) {
+            beat_clock += BPM_TO_MS(DEFAULT_TEMPO);
+        }
+        
+        // Increment out position in the beat & song
         ++bar_pos;
         if (bar_pos >= 96) {
             bar_pos = 0;
             ++song_pos;
         }
 
-        // Do led if we had 12 sub-beat timers
+        // Mark that we need to turn then next led on if we had 12 sub-beat timers
+        pos = true;
         if (bar_pos % 12 == 0) {
             beat = true;
         }
-
-        // Don't forget to advance the clock, syncing on the start of the beat
-        if (bar_pos == 0) { beat_clock = now; }
-        else { beat_clock += 1000 / ((120 / 60.0) * 24.0); }
     }
 
 
 
-    /* Step 2: Turn on the appropriate leds. */
+    /* Step 2: Possibly turn any led off. */
+    if (pos) {
+        for (int i = 0; i < 8; i++) {
+            if (GET_BOOL(note_on, i)) {
+                // Check if that caused it to turn off
+                if (--note_duration[i] == 0) {
+                    // Turn it off
+                    SET_BOOL(note_on, i, false);
+                }
+            }
+        }
+    }
+
+
+
+    /* Step 3: Turn on the appropriate leds. */
     if (beat) {
-        // Turn off the old led
-        digitalWrite(MULT2_DATA, LOW);
-
         // Set the multiplexer position
-        digitalWrite(MULT12_OUT1, (bar_pos / 12) & 0x1);
-        digitalWrite(MULT12_OUT2, ((bar_pos / 12) >> 1) & 0x1);
-        digitalWrite(MULT12_OUT3, ((bar_pos / 12) >> 2) & 0x1);
+        led = bar_pos / 12;
+        SET_MULTI(MULT12, led);
 
-        // Turn on the new led
-        digitalWrite(MULT2_DATA, HIGH);
+        // Read the potmeter above it to determine the duration of this note & set it as the note's duration
+        uint16_t raw_value = analogRead(MULT1_DATA);
+        note_stat[LENGTH][led] = raw_value / 1023.0 * 96;
+        note_duration[led] = note_stat[LENGTH][led];
+
+        // Turn on the new led if there's enough time to go
+        if (raw_value >= 15) {
+            SET_BOOL(note_on, led, true);
+        }
+    }
+
+
+
+    /* Step 4: At any given moment, write HIGH to all leds that are on. */
+    for (int i = 0; i < 7; i++) {
+        if (GET_BOOL(note_on, i)) {
+            SET_MULTI(MULT12, i);
+        }
     }
 }
